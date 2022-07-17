a990b3d90791e5d58b9529e1c9b4904ed07880b6
Fernando Guimarães <faugustus17@gmail.com>
2022-07-16 23:15:10 -0300

    Exercícios resolvidos com JS

--- 1098 - sequencia IJ4/index.js
+++ 1098 - sequencia IJ4/index.js
@@ -1,7 +1,9 @@
-let x = 0;
-let y = 1;
-for (let i = 0; i < 3; i++) {
-  for (let j = 0; j < 3; j++) {
-    console.log("I=" + i + " J=" + j);
+for (let i = 0; i <= 2; i += 0.2) {
+  for (let j = 1; j <= 3; j++) {
+    if (i == 0 || i == 1 || i > 1.8) {
+      console.log("I=" + i.toFixed(0) + " J=" + (j + i).toFixed(0));
+    } else {
+      console.log("I=" + i.toFixed(1) + " J=" + (j + i).toFixed(1));
+    }
   }
 }
